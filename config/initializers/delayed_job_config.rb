d9ee14cdf58e764fd60984171ff68b9f5b1328b2
Vignesh Prakasam <vprakasam@anpi.com>
2015-04-30 09:44:30 +0530

    delayed job workers

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,5 +4,10 @@ Delayed::Worker.destroy_failed_jobs = false
 silence_warnings do
   Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
   Delayed::Job.const_set("MAX_ATTEMPTS", 3)
+end
 
+5.times do |n|
+  worker = Delayed::Worker.new
+  worker.name = 'worker-' + n.to_s
+  worker.start
 end
