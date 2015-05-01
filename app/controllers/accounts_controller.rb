1f288f696e663e9a37da57c0d8c4b6f9b4b80e6b
Vignesh Prakasam <vprakasam@anpi.com>
2015-05-01 15:33:07 +0530

    proxy check

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -139,17 +139,20 @@ class AccountsController < ApplicationController
 
   def get_client(user)
     #user = Account.find(session[:user_id])
-    proxy = {
-        host: user.proxy,
-        port: 3128
-    }
+
+    if Rails.env != "development" and !user.proxy.blank?
+      proxy = {
+          host: user.proxy,
+          port: 3128
+      }
+    end
 
     client = Twitter::REST::Client.new do |config|
       config.consumer_key = "VcIWuB5KjBuVe4a6Guuy6wOFF"
       config.consumer_secret = "OhhaHaRG5y0e5md3Ci3wcnX6aQNDm4Qm8k604aDL0gAE7Cbj6a"
       config.access_token = user.access_token
       config.access_token_secret = user.access_secret
-      config.proxy = proxy
+      config.proxy = proxy  if proxy
     end
 
     client
